fda68dbda7d8a7bc8138407fb8c5cf1c6a02fbf7
Linlin Yan <yanlinlin82@gmail.com>
2020-09-15 16:27:19 +0800

    Update www-apps/hugo, 0.74.3 -> 0.75.1

--- utils/check-update.sh
+++ utils/check-update.sh
@@ -4,19 +4,19 @@ HUGO_VER=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | 
 if [ "$(ls www-apps/hugo/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${HUGO_VER}" ]; then
 	echo "Upgrade 'www-apps/hugo'"
 	mv -v www-apps/hugo/hugo-*.ebuild www-apps/hugo/hugo-${HUGO_VER}.ebuild
-	cd www-apps/hugo && ebuild *.ebuild digest
+	(cd www-apps/hugo && ebuild *.ebuild digest)
 fi
 
 PICARD_VER=$(curl -s https://api.github.com/repos/broadinstitute/picard/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
 if [ "$(ls sci-biology/picard/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${PICARD_VER}" ]; then
 	echo "Upgrade 'sci-biology/picard'"
 	mv -v sci-biology/picard/picard-*.ebuild sci-biology/picard/picard-${PICARD_VER}.ebuild
-	cd sci-biology/picard && ebuild *.ebuild digest
+	(cd sci-biology/picard && ebuild *.ebuild digest)
 fi
 
 CLI_VER=$(curl -s https://api.github.com/repos/cli/cli/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2 | cut -c2-)
 if [ "$(ls dev-util/github-cli/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${CLI_VER}" ]; then
 	echo "Upgrade 'dev-util/github-cli'"
 	mv -v dev-util/github-cli/github-cli-*.ebuild dev-util/github-cli/github-cli-${CLI_VER}.ebuild
-	cd dev-util/github-cli && ebuild *.ebuild digest
+	(cd dev-util/github-cli && ebuild *.ebuild digest)
 fi
