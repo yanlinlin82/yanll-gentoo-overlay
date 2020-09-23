f408f2eb8daa814527bf8f40ccc9065e5156483d
Linlin Yan <yanlinlin82@gmail.com>
2020-09-23 08:23:28 +0800

    Update sci-biology/samtools, 1.10 -> 1.11

--- utils/check-update.sh
+++ utils/check-update.sh
@@ -8,6 +8,22 @@ if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "$
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
 fi
 
+PACKAGE=sci-libs/htslib
+APP_VER=$(curl -s https://api.github.com/repos/samtools/htslib/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
+if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+	echo "Upgrade '${PACKAGE}'"
+	mv -v ${PACKAGE}/htslib-*.ebuild ${PACKAGE}/htslib-${APP_VER}.ebuild
+	(cd ${PACKAGE} && ebuild *.ebuild digest)
+fi
+
+PACKAGE=sci-biology/samtools
+APP_VER=$(curl -s https://api.github.com/repos/samtools/samtools/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
+if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+	echo "Upgrade '${PACKAGE}'"
+	mv -v ${PACKAGE}/samtools-*.ebuild ${PACKAGE}/samtools-${APP_VER}.ebuild
+	(cd ${PACKAGE} && ebuild *.ebuild digest)
+fi
+
 PACKAGE=sci-biology/picard
 APP_VER=$(curl -s https://api.github.com/repos/broadinstitute/picard/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
 if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
