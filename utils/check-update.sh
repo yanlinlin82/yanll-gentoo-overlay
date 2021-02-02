50e2592a71d3d9552e4eb84481701c0c6f244e72
Linlin Yan <yanlinlin82@gmail.com>
2021-02-02 12:35:02 +0800

    Improve script

--- utils/check-update.sh
+++ utils/check-update.sh
@@ -2,7 +2,7 @@
 
 PACKAGE=www-apps/hugo
 APP_VER=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2 | cut -c2-)
-if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+if [ -n "${APP_VER}" -a "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
 	echo "Upgrade '${PACKAGE}'"
 	mv -v ${PACKAGE}/hugo-*.ebuild ${PACKAGE}/hugo-${APP_VER}.ebuild
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
@@ -10,7 +10,7 @@ fi
 
 PACKAGE=sci-libs/htslib
 APP_VER=$(curl -s https://api.github.com/repos/samtools/htslib/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
-if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+if [ -n "${APP_VER}" -a "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
 	echo "Upgrade '${PACKAGE}'"
 	mv -v ${PACKAGE}/htslib-*.ebuild ${PACKAGE}/htslib-${APP_VER}.ebuild
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
@@ -18,7 +18,7 @@ fi
 
 PACKAGE=sci-biology/samtools
 APP_VER=$(curl -s https://api.github.com/repos/samtools/samtools/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
-if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+if [ -n "${APP_VER}" -a "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
 	echo "Upgrade '${PACKAGE}'"
 	mv -v ${PACKAGE}/samtools-*.ebuild ${PACKAGE}/samtools-${APP_VER}.ebuild
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
@@ -26,7 +26,7 @@ fi
 
 PACKAGE=sci-biology/picard
 APP_VER=$(curl -s https://api.github.com/repos/broadinstitute/picard/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2)
-if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+if [ -n "${APP_VER}" -a "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
 	echo "Upgrade '${PACKAGE}'"
 	mv -v ${PACKAGE}/picard-*.ebuild ${PACKAGE}/picard-${APP_VER}.ebuild
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
@@ -34,7 +34,7 @@ fi
 
 PACKAGE=sci-biology/bowtie
 APP_VER=$(curl -s https://api.github.com/repos/BenLangmead/bowtie2/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2 | cut -c2-)
-if [ "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
+if [ -n "${APP_VER}" -a "$(ls ${PACKAGE}/ | grep ebuild | cut -d- -f2 | sed 's/\.ebuild$//')" != "${APP_VER}" ]; then
 	echo "Upgrade '${PACKAGE}'"
 	mv -v ${PACKAGE}/bowtie-*.ebuild ${PACKAGE}/bowtie-${APP_VER}.ebuild
 	(cd ${PACKAGE} && ebuild *.ebuild digest)
